905e25048dc1868366d47c38a6c1f8509defd92a
andres holivin <30800318+holivin21@users.noreply.github.com>
2022-01-30 21:33:29 +0700

    add ads

--- ListTo/proguard-rules.pro
+++ ListTo/proguard-rules.pro
@@ -30,8 +30,13 @@
 -keep class com.huawei.updatesdk.**{*;}
 -keep class com.huawei.hms.**{*;}
 -keep class com.hianalytics.android.**{*;}
+-keep class com.huawei.openalliance.ad.** { *; }
+-keep class com.huawei.hms.ads.** { *; }
 
 
--keepattributes SourceFileLineNumberTable,
+-keepattributes SourceFileLineNumberTable
 
 
+
+-keep class com.huawei.hms.ads.** {*; }
+-keep interface com.huawei.hms.ads.** {*; }
--- ListTo/src/main/java/com/example/listto/MainActivity.java
+++ ListTo/src/main/java/com/example/listto/MainActivity.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.listto.model.DatabaseHandler;
 import com.example.listto.model.TaskModel;
@@ -19,6 +20,11 @@ import com.huawei.hmf.tasks.OnFailureListener;
 import com.huawei.hmf.tasks.OnSuccessListener;
 import com.huawei.hmf.tasks.Task;
 import com.huawei.hms.aaid.HmsInstanceId;
+import com.huawei.hms.ads.AdListener;
+import com.huawei.hms.ads.AdParam;
+import com.huawei.hms.ads.BannerAdSize;
+import com.huawei.hms.ads.HwAds;
+import com.huawei.hms.ads.banner.BannerView;
 import com.huawei.hms.common.ApiException;
 import com.huawei.hms.push.HmsMessaging;
 import com.huawei.hms.support.account.AccountAuthManager;
@@ -28,6 +34,7 @@ import com.huawei.hms.support.account.result.AuthAccount;
 import com.huawei.hms.support.account.service.AccountAuthService;
 
 import java.util.Calendar;
+import java.util.Locale;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -36,12 +43,16 @@ public class MainActivity extends AppCompatActivity {
     private AccountAuthParams mAuthParam;
     private static final int REQUEST_CODE_SIGN_IN = 1003;
     private static final String TAG = "Account";
-
+    private BannerView defaultBannerView;
+    private static final int REFRESH_TIME = 60;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        HwAds.init(this);
+        loadDefaultBannerAd();
+
         findViewById(R.id.HuaweiIdAuthButton).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -54,6 +65,60 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(mAuthService.getSignInIntent(), REQUEST_CODE_SIGN_IN);
             }
         });
+    }
+    private void loadDefaultBannerAd() {
+        defaultBannerView=new BannerView(this);
+        // Obtain BannerView based on the configuration in layout/activity_main.xml.
+        defaultBannerView = findViewById(R.id.hw_banner_view);
+        defaultBannerView.setAdListener(adListener);
+        defaultBannerView.setBannerRefresh(REFRESH_TIME);
+        defaultBannerView.setBannerRefresh(30);
+//        defaultBannerView.setAdId("teste9ih9j0rc3");
+        defaultBannerView.setBannerAdSize(BannerAdSize.BANNER_SIZE_360_57);
+
+        AdParam adParam = new AdParam.Builder().build();
+        defaultBannerView.loadAd(adParam);
+    }
+    private AdListener adListener = new AdListener() {
+        @Override
+        public void onAdLoaded() {
+            // Called when an ad is loaded successfully.
+            showToast("Ad loaded.");
+        }
+
+        @Override
+        public void onAdFailed(int errorCode) {
+            // Called when an ad fails to be loaded.
+            showToast(String.format(Locale.ROOT, "Ad failed to load with error code %d.", errorCode));
+        }
+
+        @Override
+        public void onAdOpened() {
+            // Called when an ad is opened.
+            showToast(String.format("Ad opened "));
+        }
+
+        @Override
+        public void onAdClicked() {
+            // Called when a user taps an ad.
+            showToast("Ad clicked");
+        }
+
+        @Override
+        public void onAdLeave() {
+            // Called when a user has left the app.
+            showToast("Ad Leave");
+        }
+
+        @Override
+        public void onAdClosed() {
+            // Called when an ad is closed.
+            showToast("Ad closed");
+        }
+    };
+    private void showToast(String message) {
+//        Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+        Log.e("status", message );
     }
     private void setReceiveNotifyMsg(final boolean enable) {
         showLog("Control the display of notification messages:begin");
